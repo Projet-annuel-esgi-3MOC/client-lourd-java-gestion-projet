7872072a96272c7fba1fbf8d241fb938de1be884
Agence 369 <dev@agence369.com>
2023-03-09 21:16:52 +0100

    default javaFX application

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module fr.esgi.projetannuelgui {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+//    requires eu.hansolo.tilesfx;
+
+    opens fr.esgi.projetannuelgui to javafx.fxml;
+    exports fr.esgi.projetannuelgui;
+}
